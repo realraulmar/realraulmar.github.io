2e8a48aa8323931b650bc85c098f376a49bab53a
raulmar <raulhdz40@gmail.com>
2020-12-22 06:21:25 -0600

    Work In Progress page ready

---
+++ index.js
@@ -0,0 +1,58 @@
+const PHRASES = [
+  'Changing Layout...',
+  'Choosing color palette...', 
+  'Improving performace...', 
+  'Updating projects...'
+]
+
+let i = 0
+
+class TypingPhrases {
+  
+  constructor() {
+    this.phraseContainer = document.querySelector('.phrase')
+    window.onload = this.initPhrases()
+  }
+
+  initPhrases() {
+    this.phrases = [...PHRASES]
+    
+    setInterval(() => {
+      this.renderPhrase()
+    },0)
+  }
+  
+  renderPhrase() {
+    if(this.currentPhrase) {
+      return
+    }
+    const phrase = this.getPhrase()
+    this.renderLetters(phrase)
+    this.currentPhrase = phrase
+    
+    setTimeout(() => {
+      this.currentPhrase = null
+      this.phraseContainer.innerHTML = ''
+      i = 0
+    }, 3000)
+  }
+
+  getPhrase() {
+    if(this.phrases.length === 0) {
+      this.initPhrases()
+    }
+    return this.phrases.pop()
+  }
+
+  renderLetters(phrase) {
+    if(i < phrase.length) {
+      // debugger
+      this.phraseContainer.innerHTML += phrase.charAt(i)
+      i++
+      setTimeout(() => {this.renderLetters(phrase)},100)
+    }
+    return
+  }
+}
+
+const startTyping = new TypingPhrases()
